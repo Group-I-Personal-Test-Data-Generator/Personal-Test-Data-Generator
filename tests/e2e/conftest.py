89c1d42389a71a4d2b0cd2079efc20be52f018a9
marcus_rk <makj0005@stud.kea.dk>
2025-10-22 11:41:09 +0200

    Fixed Headless

--- tests/e2e/conftest.py
+++ tests/e2e/conftest.py
@@ -6,7 +6,7 @@ BASE_URL = "http://127.0.0.1:5500/frontend/index.html"
 @pytest.fixture
 def browser_page():
     with sync_playwright() as p:
-        browser = p.chromium.launch(headless=False)
+        browser = p.chromium.launch(headless=True)
         page = browser.new_page()
         page.goto(BASE_URL)
         yield page
